d7742835e8fc965fb2afde10faca4d5e2f84b9c0
LeeSY0110 <112144874+LeeSY0110@users.noreply.github.com>
2023-09-14 22:59:01 +0800

    Update company.py

--- company.py
+++ company.py
@@ -26,7 +26,7 @@ table = 'company'
 #if call / then will redirect to that pg
 
 #new
-@app.route("/", methods=['POST'])
+@app.route("/")
 def StudRegister():
     cohort = request.form['cohort']
     internPeriod = request.form['internPeriod']
